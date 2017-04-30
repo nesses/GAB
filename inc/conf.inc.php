f2168e4d7cd21a3d60f3f3ace493bc2f1151acc8
nesses <nesses@thinkpad>
2017-05-01 00:00:09 +0200

    starting refactoring with rewriten database class

--- inc/conf.inc.php
+++ inc/conf.inc.php
@@ -3,6 +3,11 @@ $db_type = "SQLite";
 
 $db_conf = "gab.sqlite";
 
+$mysql_conf = [ 'host'      => 'localhost',
+                'dbname'    => 'gab',
+                'user'      => 'root',
+                'passwd'    => ''];
+
 $modules = ARRAY(   "Login" => "login",
                     "Mitarbeiter" => "employees");
 
