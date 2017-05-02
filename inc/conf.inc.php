6964af5ed7125cd1e96acefa254d86c08d99fcf4
nesses <matthias.grotjohann@gmail.com>
2017-05-02 10:28:49 +0200

    mobile

--- inc/conf.inc.php
+++ inc/conf.inc.php
@@ -5,6 +5,6 @@ $db_conf = [ "SQLite"   =>  ["filename"  => "gab.sqlite"],
              "MySQL"    =>  [ 'host'      => 'localhost',
                               'dbname'    => 'gab',
                               'user'      => 'root',
-                              'passwd'    => '08xb7cX2']];
+                              'passwd'    => '']];
 
 ?>
